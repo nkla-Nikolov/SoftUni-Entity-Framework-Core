8f92e68afb79d37a6470dc7e66a58a11e2dabfc5
Nikola Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-06-20 11:46:08 +0300

    Initial commit

---
+++ Entity Relations/P01_StudentSystem/Data/Models/Resource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        public int ResourceId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(max)")]
+        public string Url { get; set; }
+
+        [Required]
+        public ResourceType ResourceType { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
