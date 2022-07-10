fede26e09e2e7dbb5df19bdf36c3b92a28679cff
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-07-10 22:45:36 +0300

    Initial commit

---
+++ Extensible Markup Language - XML/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProductShop.Dtos;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<UserDto, User>();
+
+            CreateMap<ProductDto, Product>();
+
+            CreateMap<CategoryDto, Category>();
+
+            CreateMap<CategoryProductDto, CategoryProduct>();
+        }
+    }
+}
