115dce6b56664d2050bec95ca21f34069039f226
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-07-12 11:35:50 +0300

    Initial commit

---
+++ Extensible Markup Language - XML/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CarDealer.Models;
+using CarDealer.Dtos.Import;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<SupplierDto, Supplier>();
+
+            CreateMap<PartDto, Part>();
+
+            CreateMap<CarDto, Car>();
+
+            CreateMap<CustomerDto, Customer>();
+
+            CreateMap<SaleDto, Sale>();
+        }
+    }
+}
