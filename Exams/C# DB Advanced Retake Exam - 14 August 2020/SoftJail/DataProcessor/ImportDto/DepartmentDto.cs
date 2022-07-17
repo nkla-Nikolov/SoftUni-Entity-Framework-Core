cbd027ba4c9dfeb90f345787cba2a134935fcab6
Nikola Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-07-17 22:20:25 +0300

    Initial commit

---
+++ Exams/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/ImportDto/DepartmentDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class DepartmentDto
+    {
+        public DepartmentDto()
+        {
+            this.Cells = new List<CellDto>();
+        }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        public List<CellDto> Cells { get; set; }
+    }
+}
---
+++ Exams/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/ImportDto/OfficerPrisonerDto.cs
@@ -0,0 +1,43 @@
+﻿using SoftJail.Data.Models;
+using SoftJail.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class OfficerPrisonerDto
+    {
+        public OfficerPrisonerDto()
+        {
+            this.Prisoners = new List<PrisonerIdDto>();
+        }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Money")]
+        [Range(0, double.MaxValue)]
+        public decimal Money { get; set; }
+
+        [Required]
+        [XmlElement("Position")]
+        public string Position { get; set; }
+
+        [Required]
+        [XmlElement("Weapon")]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public List<PrisonerIdDto> Prisoners { get; set; }
+    }
+}
