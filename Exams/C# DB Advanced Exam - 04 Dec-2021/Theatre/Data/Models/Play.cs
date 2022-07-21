d2d3817d1658d220d9bdc073fb691e0a5359e9b7
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-07-21 23:40:39 +0300

    Initial commit

---
+++ Exams/C# DB Advanced Exam - 04 Dec-2021/Theatre/Data/Models/Play.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.Data.Models
+{
+    public class Play
+    {
+        public Play()
+        {
+            this.Tickets = new HashSet<Ticket>();
+            this.Casts = new HashSet<Cast>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public float Rating { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [MaxLength(700)]
+        public string Description { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Screenwriter { get; set; }
+
+        public ICollection<Cast> Casts { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
